b4922f327f84bc6bcf2f38fcc04bf3e4b7bb823a
Fabian Bliem <fabianbliem@icloud.com>
2021-11-07 01:44:20 +0100

    feat: added aboutPage & fixed firebase

---
+++ src/app/components/StyledLink/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components/macro';
+import { Link as RouterLink } from 'react-router-dom';
+
+export const StyledLink = styled(RouterLink)`
+  text-decoration: none;
+  background-color: #ffffff;
+  color: #333333;
+  padding: 8px 40px 8px 40px;
+  border-top: 1px solid #333333;
+  border-right: 1px solid #333333;
+  border-bottom: 1px solid #333333;
+  border-left: 1px solid #333333;
+  border-radius: 3px;
+  text-align: center;
+`;
