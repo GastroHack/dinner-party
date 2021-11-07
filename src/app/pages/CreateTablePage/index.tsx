2f768311a2d859a251a6f31ecf2d25f27f006b76
Sergii Kauk <sergii@kauk.at>
2021-11-07 09:07:44 +0100

    Add a background with a hook

--- src/app/pages/CreateTablePage/index.tsx
+++ src/app/pages/CreateTablePage/index.tsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { PageWrapper } from 'app/components/PageWrapper';
 import ConnectedCreateTableHooks from './CreateTableHooks';
+import { useEffect } from 'react';
 
 export function CreateTablePage() {
+
+  useEffect(()  => {
+    document.body.classList.add('bg-green');
+
+    return () => {
+      document.body.classList.remove('bg-green');
+    };
+  });
+
   return (
     <>
       <Helmet>
@@ -11,7 +21,7 @@ export function CreateTablePage() {
         <meta name="description" content="Create a Table Page" />
       </Helmet>
 
-      <PageWrapper className="bg-green">
+      <PageWrapper>
         <ConnectedCreateTableHooks />
       </PageWrapper>
     </>
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { PageWrapper } from 'app/components/PageWrapper';
 import ConnectedHomeHooks from './HomeHooks';
+import { useEffect } from 'react';
 
 export function HomePage() {
+
+  useEffect(()  => {
+    document.body.classList.add('bg-green');
+
+    return () => {
+      document.body.classList.remove('bg-green');
+    };
+  });
+
   return (
     <>
       <Helmet>
